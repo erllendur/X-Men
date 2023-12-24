4f126cb6da8e4db703b3c31e9581e11932994779
Matheus <matheusmourapereira@outlook.com>
2023-12-24 00:16:05 -0300

    OrganizandoOsLados

--- src/js/index.js
+++ src/js/index.js
@@ -2,36 +2,32 @@ const personagemSelecionadoEsquerda = document.querySelector('.personagem-seleci
 const personagemSelecionadoDireita = document.querySelector('.personagem-selecionado-direita');
 const personagens = document.querySelectorAll('.personagem');
 let mouseEnterAtivo = true;
-let travarLadoEsquerdo = true;
+let travarLadoEsquerdo = false;
+let travarTudo = false;
+let travarLadoDireito = false
 
-function atualizarInformacoes(personagem) {
-  const imagemGrandeEsquerda = personagemSelecionadoEsquerda.querySelector('.personagem-grande');
-  const imagemGrandeDireita = personagemSelecionadoDireita.querySelector('.personagem-grande');
-
-  const nomePersonagemEsquerda = personagemSelecionadoEsquerda.querySelector('.nome-personagem');
-  const nomePersonagemDireita = personagemSelecionadoDireita.querySelector('.nome-personagem');
-
-  const descricaoPersonagemEsquerda = personagemSelecionadoEsquerda.querySelector('.descricao-personagem');
-  const descricaoPersonagemDireita = personagemSelecionadoDireita.querySelector('.descricao-personagem');
-
-  const idPersonagem = personagem.getAttribute('id');
-  const nomePersonagem = personagem.getAttribute('data-name');
-  const descricaoPersonagem = personagem.getAttribute('data-description');
-
-  imagemGrandeEsquerda.src = `src/imagens/card-${idPersonagem}.png`;
-  imagemGrandeDireita.src = `src/imagens/card-${idPersonagem}.png`;
-
-  nomePersonagemEsquerda.innerText = nomePersonagem;
-  nomePersonagemDireita.innerText = nomePersonagem;
-
-  descricaoPersonagemEsquerda.innerText = descricaoPersonagem;
-  descricaoPersonagemDireita.innerText = descricaoPersonagem;
-}
+personagens.forEach((personagem) => {
+  personagem.addEventListener('mouseenter', mouseEnterHandler);
+  personagem.addEventListener('click', () => {
+    if(travarLadoEsquerdo){
+      travarLadoDireito = true
+      travarTudo = true
+    }
+    travarLadoEsquerdo = true
+    
+    // mouseEnterHandler(event);
+    // desativarMouseEnter();
+  });
+});
 
 function mouseEnterHandler(event) {
+  if (travarTudo) {
+    return
+  }
   if (mouseEnterAtivo) {
     const personagem = event.currentTarget;
-    atualizarInformacoes(personagem);
+
+    atualizarInformacoes(personagem, travarLadoEsquerdo);
 
     if (window.innerWidth < 450) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -48,24 +44,44 @@ function mouseEnterHandler(event) {
   }
 }
 
-function desativarMouseEnter() {
-  mouseEnterAtivo = false;
-}
+function atualizarInformacoes(personagem, side) {
+  const imagemGrandeEsquerda = personagemSelecionadoEsquerda.querySelector('.personagem-grande');
+  const nomePersonagemEsquerda = personagemSelecionadoEsquerda.querySelector('.nome-personagem');
+  const descricaoPersonagemEsquerda = personagemSelecionadoEsquerda.querySelector('.descricao-personagem');
 
-function ativarMouseEnter() {
-  mouseEnterAtivo = true;
-}
 
-personagens.forEach((personagem) => {
-  personagem.addEventListener('mouseenter', mouseEnterHandler);
-  personagem.addEventListener('click', () => {
-    mouseEnterHandler(event);
-    desativarMouseEnter();
-  });
-});
+  const imagemGrandeDireita = personagemSelecionadoDireita.querySelector('.personagem-grande');
+  const nomePersonagemDireita = personagemSelecionadoDireita.querySelector('.nome-personagem');
+  const descricaoPersonagemDireita = personagemSelecionadoDireita.querySelector('.descricao-personagem');
 
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
-    ativarMouseEnter();
+  const idPersonagem = personagem.getAttribute('id');
+  const nomePersonagem = personagem.getAttribute('data-name');
+  const descricaoPersonagem = personagem.getAttribute('data-description');
+
+  if (side == false) {
+    imagemGrandeEsquerda.src = `src/imagens/card-${idPersonagem}.png`;
+    nomePersonagemEsquerda.innerText = nomePersonagem;
+    descricaoPersonagemEsquerda.innerText = descricaoPersonagem;
+  } else {
+    imagemGrandeDireita.src = `src/imagens/card-${idPersonagem}.png`;
+    nomePersonagemDireita.innerText = nomePersonagem;
+    descricaoPersonagemDireita.innerText = descricaoPersonagem;
+    travarLadoDireito = true
+    
   }
-});
+}
+
+// function desativarMouseEnter() {
+//   mouseEnterAtivo = false;
+// }
+
+// function ativarMouseEnter() {
+//   mouseEnterAtivo = true;
+// }
+
+
+ document.addEventListener('keydown', (event) => {
+ if (event.key === 'Escape') {
+     ativarMouseEnter();
+   }
+ });
